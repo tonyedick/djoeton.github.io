223303756f548c9a05d79d973406ebad93d53bc8
Dick Tonye <31224101+tonyedick@users.noreply.github.com>
2022-03-03 20:25:50 +0100

    Course details page

---
+++ src/components/CoursesPathDetails/CoursesPathDetails.jsx
@@ -0,0 +1,20 @@
+import React, { Component, Fragment } from 'react'
+import { Col, Container, Row } from 'react-bootstrap'
+
+class CoursesPathDetails extends Component {
+        render() {
+            return(
+                <Fragment>
+                    <Container>
+                        <Row>
+                            <Col>
+                                <h1>Choose Path</h1>
+                            </Col>
+                        </Row>
+                    </Container>
+                </Fragment>
+            )
+        }
+}
+
+export default CoursesPathDetails
